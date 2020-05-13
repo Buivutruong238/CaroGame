1cf997c6584a90353666c2e526b630995458b310
vietsaclo <vietsaclo@gmail.com>
2020-05-13 18:03:53 +0700

    xong phan giao dien

--- BuiVuTruong_2001170208_BTVN_Buoi1/Form1.cs
+++ BuiVuTruong_2001170208_BTVN_Buoi1/Form1.cs
@@ -12,16 +12,46 @@ namespace BuiVuTruong_2001170208_BTVN_Buoi1
 {
     public partial class Form1 : Form
     {
+        private Button[,] banCo;
+        private int size;
+
         public Form1()
         {
             InitializeComponent();
+
+            size = 10;
+            banCo = new Button[size, size];
+            addControls();
         }
 
         private void addControls()
         {
-            Label lbl = new Label();
-            MessageBox.Show("123");
+            Button btn;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    btn = createButton(j * 40, i * 40);
+                    pnBanCo.Controls.Add(btn);
+                    banCo[i, j] = btn;
+                }
+            }
         }
 
+        private Button createButton(int x, int y)
+        {
+            Button btn = new Button();
+            btn.Width = btn.Height = 40;
+            btn.Location = new Point(x, y);
+            btn.Click += new System.EventHandler(oCoClicked);
+
+            return btn;
+        }
+
+        private void oCoClicked(object sen, EventArgs ev)
+        {
+            Button btn = (Button)sen;
+            btn.BackColor = Color.LightBlue;
+        }
     }
 }
